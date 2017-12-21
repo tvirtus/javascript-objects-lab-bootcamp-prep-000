73b0d46ee0882ce8290399a373b6e4cd5d6c330f
Victor Trofimciuc <victortrofimciuc@gmail.com>
2017-12-21 21:34:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function deleteFromObjectByKey(obj, key) {
   delete newObj[key];
   return newObj;
 }
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj[key];
+  return obj;
+}
